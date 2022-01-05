4a1d0cbcf9628c05799c04e9a9d12c061105d640
kelloy <keldren.loy@gmail.com>
2022-01-05 14:46:09 +0800

    new

---
+++ src/test/java/tfip/Workshop11/MyApplicationTests.java
@@ -0,0 +1,13 @@
+package tfip.Workshop11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
